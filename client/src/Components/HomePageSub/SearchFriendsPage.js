07805c2b63984025fba967f8960d6a83203bb3b5
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2020-08-24 15:47:12 -0600

    updated search functionality

--- client/src/Components/HomePageSub/SearchFriendsPage.js
+++ client/src/Components/HomePageSub/SearchFriendsPage.js
@@ -1,47 +1,38 @@
-import React from "react"
+import React, {useState} from "react"
+import {useSelector} from "react-redux"
 import "../../CSS/SearchFriendsPage.css"
 
 const SearchFriendPage = () => {
-    const [clicked, setClicked] = React.useState(false)
+    const [searchValue, setSearchValue] = useState("")
+    const friendsList = useSelector(state=>state.friendsList)
+    const onSearchChange = event => {
+        setSearchValue(event.target.value)
+    }
+    const isSearchValueEmpty = () => {
+        return searchValue === ""
+    }
 
-        if (clicked) {
-            console.log("clicked is true")
-            return (
-                <div className="searchfriendpage">
-                    <div className="clickedsearchbox">
-                        <form>
-                        <i className="fas fa-search" type="submit"></i>
-                            <input onClick={()=>{setClicked(true)}}
-                            >
+    const searchResults = friendsList.filter(friend=>(friend.name.first.indexOf(searchValue) !== -1) || (friend.name.last.indexOf(searchValue) !== -1)).map(friend=>{
+        return (
+        <div className="individualresults"><img alt="friend" src={friend.picture.large}></img><span>{friend.name.first} {friend.name.last}</span></div>
+            )
+    })
 
-                            </input>
-
-                        </form>
-                    </div>
-                <div className="results">
-
-                </div>
+    return (
+        <div className="searchfriendpage">
+            <div className={`searchbox ${isSearchValueEmpty()? 'notclickedsearchbox': 'clickedsearchbox'}`}>
+                <form>
+                <i className="fas fa-search" type="submit"></i>
+                    <input
+                        value={searchValue}
+                        onChange={onSearchChange}>
+                    </input>
+                </form>
             </div>
-            )
-        } else {
-            return (
-                <div className="searchfriendpage">
-                    <div className="searchbox">
-                        <form>
-                        <i className="fas fa-search" type="submit"></i>
-                            <input onClick={()=>{setClicked(true)}}
-                            >
-
-                            </input>
-
-                        </form>
-                    </div>
-                    <div className="results">
-
-                    </div>
-                </div>
-            )
-        }
+            <div className={`${isSearchValueEmpty()? 'notclickedresults': 'results'}`}>
+                {!isSearchValueEmpty()?searchResults:null}
+            </div>
+        </div>
+    )
 }
-
 export default SearchFriendPage
